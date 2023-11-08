7073c46d6dc34729583f51b652496e4fead6f531
EdwartAndres <125517012+EdwartAndres@users.noreply.github.com>
2023-11-08 00:23:28 -0500

    11 commit

---
+++ back/src/main/java/com/example/demo/controller/HomeController.java
@@ -0,0 +1,28 @@
+package com.example.demo.controller;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String home(Model model, @AuthenticationPrincipal OidcUser principal) {
+        return "index";
+    }
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @PostMapping("/login")
+    public String login(Authentication authentication) {
+        // Redirect to the home page after successful login
+        return "redirect:/";
+    }
+}
+
